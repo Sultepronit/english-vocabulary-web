f8604140f89126ea7272b9c0a7828f8397376c27
Sultepronit <sultepronit@gmail.com>
2023-03-30 19:43:06 +0300

    created module for functions out of cycle

---
+++ javascripts/functions.js
@@ -0,0 +1,29 @@
+function randomFromRange(from, to) {
+	return Math.round((Math.random() * (to - from)) + from);
+}
+
+function showInput() {
+	$("input").val("");
+	$("input").show();
+	$("input").focus();
+	$(".typed-in").hide();
+}
+
+function hideInput() {
+	$("input").hide();
+	$(".typed-in").text(" ");
+	$(".typed-in").show();
+}
+
+function playSound() {
+	if(progress == "TRAINING" || "TYPE_IN_ANSWER") return;
+	speakGenerate();
+}
+
+function speakGenerate() {
+	let utterance = new SpeechSynthesisUtterance(currentCard[5]);
+	utterance.lang = 'en';
+	utterance.rate = 0.7;
+	speechSynthesis.speak(utterance);
+}
+
